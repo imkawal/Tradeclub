0be0facd4d08a591f787bd8d380c9c1c45da973a
DevTradeClub <tradeclub159@gmail.com>
2023-09-10 15:58:31 +0530

    ecom web changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter as Router } from 'react-router-dom';
 
+function Test() {
+  const path = window.location.pathname;
+  const type = path.slice(1, 5);
+  return type;
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Router>
-        <App />
-    </Router>
+console.log(Test());
+
+if (Test() != 'ecom') {
+  var elementToRenderIn = document.getElementById('root'); // Render in 'root'
+  var ecomElement = document.getElementById('ecom'); // Get the 'ecom' element
+  ecomElement.innerHTML = ''; // Remove the inner HTML of 'ecom'
+} else {
+  var elementToRenderIn = document.getElementById('customroot'); // Render in 'ecom'
+}
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  elementToRenderIn
 );
 
 // If you want to start measuring performance in your app, pass a function
